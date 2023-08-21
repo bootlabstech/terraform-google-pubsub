7ac5abc5eef5da2d6b1af3198c123312e5f679b0
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-21 20:11:33 +0530

    latest provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">=0.13"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.1.0"
+      version = "4.78.0"
     }
   }
 }
